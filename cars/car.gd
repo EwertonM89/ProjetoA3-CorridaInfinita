343288e8350698e7596f6f72cc1a083463944cab
Ewerton <tonmoreira7@gmail.com>
2024-06-16 21:38:17 -0300

    ajuste e adição do kit de carros

--- cars/car.gd
+++ cars/car.gd
@@ -1,26 +1,44 @@
 extends VehicleBody3D
 
-# Define a velocidade constante
-const CONSTANT_SPEED = 500
-var wheels = []
+# Defina a velocidade constante para frente
+const FORWARD_SPEED = 30.0
+# Defina a força para virar o veículo
+const STEERING_FORCE = 0.5
+
+# Referências para as rodas
+var steering_wheels = [$FrenteDireita, $FrenteEsquerda]
+var traction_wheels = [$AtrasDireita, $AtrasEsquerda]
+
+func _ready():
+	# Encontre as rodas e armazene referências
+	for child in get_children():
+		if child is VehicleWheel3D:
+			if child.use_as_steering:
+				steering_wheels.append(child)
+			if child.use_as_traction:
+				traction_wheels.append(child)
+	
+	# Definir propriedades das rodas
+	for wheel in steering_wheels:
+		wheel.use_as_steering = true
+	for wheel in traction_wheels:
+		wheel.use_as_traction = true
 
 func _process(delta):
-# Captura o valor de direção dos inputs do usuário
-	steering = Input.get_axis("ui_right", "ui_left") * .2
+	# Capturar a entrada do teclado
+	var steering_input = 0.0
+	if Input.is_action_pressed("ui_left"):
+		steering_input = -1.0
+	elif Input.is_action_pressed("ui_right"):
+		steering_input = 1.0
 
-# Define uma força constante para mover o veículo para frente
-	engine_force = CONSTANT_SPEED
+	# Ajustar a direção do veículo
+	for wheel in steering_wheels:
+		wheel.steering = steering_input * STEERING_FORCE
 
-# Aplica a força do motor e a direção ao veículo
-	apply_engine_force(engine_force)
-	apply_steering(steering)
+	# Aplicar a velocidade constante para frente
+	apply_engine_force(FORWARD_SPEED)
 
-# Função para aplicar a força do motor ao veículo
 func apply_engine_force(force):
-	for wheel in wheels:
+	for wheel in traction_wheels:
 		wheel.engine_force = force
-
-# Função para aplicar a direção ao veículo
-func apply_steering(steer_value):
-	for wheel in wheels:
-		wheel.steering = steer_value
