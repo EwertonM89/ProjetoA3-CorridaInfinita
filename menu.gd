73fd9fda6c2a6764ea10c9c65b6c2ff4a8be8f58
Lagostini <95289992+LagostaEX@users.noreply.github.com>
2024-06-18 18:09:37 -0300

    Retirada dos creditos

--- menu.gd
+++ menu.gd
@@ -6,8 +6,5 @@ func _ready():
 func _on_start_pressed():
 	get_tree().change_scene_to_file("res://Word.tscn")
 
-func _on_options_pressed():
-	get_tree().change_scene_to_file("res://Creditos.tscn")
-
 func _on_quit_pressed():
 	get_tree().quit()
